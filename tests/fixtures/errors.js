cb8178cecad4232980ac0ce5f3343afdb41acb85
rodolphebarbanneau <46493454+rodolphebarbanneau@users.noreply.github.com>
2024-02-06 19:13:42 +0100

    🚀 Upload live demo files.

--- tests/fixtures/errors.js
+++
@@ -1,8 +0,0 @@
-export class NoEntryError extends Error {
-  constructor() {
-    super();
-    this.name = 'NoEntryError';
-    this.code = 'ENOENT';
-    this.message = 'No such file or directory';
-  }
-}
